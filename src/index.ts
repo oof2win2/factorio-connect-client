c1f723070ff0e63fbd06ab15e594189f014705c6
oof2win2 <honza.koco44@gmail.com>
2021-12-18 23:05:30 +0100

    :sparkles: joining a game done

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,54 @@
-const FACTORIO_VERSION = "1.1.49.59227" as const;
+type VERSION = `${number}.${number}.${number}`
+interface GlobalData {
+	connectionRequestIDGeneratedOnClient: number
+	connectionRequestIDGeneratedOnServer?: number
+	instanceID: number
+	username: string
+	gamePassword: string
+	serverKey: string
+	serverKeyTimestamp: string
+	coreChecksum: number
+	prototypeListChecksum: number
+	mods: {
+		name: string,
+		version: VERSION
+		crc: number
+	}[]
+	/**
+	 * sequence number. increments by one every time something is sent
+	 */
+	sequenceNumberToSend: number
+}
+
+const FACTORIO_VERSION: VERSION = "1.1.49.59227" as const;
 const REMOTE_ADDR = "144.76.101.206" as const;
 const REMOTE_PORT = 39000 as const;
 const version = FACTORIO_VERSION
   .split(".")
   .map((x) => parseInt(x));
 
-interface GlobalData {
-	connectionRequestIDGeneratedOnClient?: number
-	connectionRequestIDGeneratedOnServer?: number
-}
 const globalData: GlobalData = {
-
+	connectionRequestIDGeneratedOnClient: 0x1234567b,
+	instanceID: 1923959348,
+	username: "oof2win3",
+	gamePassword: "",
+	serverKey: "qXIuKKtimqugRwdCLV9gQw==",
+	serverKeyTimestamp: "",
+	coreChecksum: 1625176034,
+	prototypeListChecksum: 3034860339,
+	mods: [{
+		name: "base",
+		version: "1.1.49",
+		crc: 3210524035
+	}],
+	sequenceNumberToSend: NaN,
 }
 
-
-globalData.connectionRequestIDGeneratedOnClient = 3369486983
+const REMOTE = {
+	hostname: REMOTE_ADDR,
+	port: REMOTE_PORT,
+	transport: "udp",
+} as const
 
 
 function printBytes(bytes: Uint8Array | Uint16Array): void {
@@ -44,6 +78,14 @@ function u8ToNum(u8: Uint8Array): number {
 	}
 	return parseInt(str, 16)
 }
+
+function stringToU8(string: string): number[] {
+	const output = []
+	for (let i = 0; i < string.length; i++) {
+	  output.push(string.charCodeAt(i))
+	}
+	return output
+  }
 
 /**
  * switch endians of a number
@@ -77,7 +119,7 @@ const listener = Deno.listenDatagram({
 	hostname: "0.0.0.0",
 	port: 39000,
 	transport: "udp",
-  });
+});
 
 
 async function sendConnectionRequest() {
@@ -94,11 +136,7 @@ async function sendConnectionRequest() {
 		...numToU8(switchEndians(globalData.connectionRequestIDGeneratedOnClient))
 	])
 
-	await listener.send(ConnectionRequest, {
-		hostname: REMOTE_ADDR,
-		port: REMOTE_PORT,
-		transport: "udp",
-	})
+	await listener.send(ConnectionRequest, REMOTE)
 	const [reply] = await listener.receive()
 	if (
 		reply[0].toString(16)[1] === "3" && // type=ConnectionRequestReply
@@ -115,26 +153,124 @@ async function sendConnectionRequest() {
 async function sendConnectionRequestReplyConfirm() {
 	// connection request reply format is:
 	// flags: 0x44
-	// filler 2X 0x00
+	// header confirm: 0x01 0x00
 	// clientConnectionID
 	// serverConnectionID
-	// then some other data, need to figure out. maybe from CVDump.exe
+	// instance ID - maybe 
+	// username length
+	// username
+	// password hash length (game password) - 0 if no password
+	// password
+	// server key length - 0 if no password?
+	// server key
+	// server key timestamp length - seems to be 0 on regular?
+	// server key timestamp
+	// core checksum - 1625176034 for dev-dump?
+	// prototype list checksum - 3034860339 for base game?
+	// active mods size (including base)
+	// for each mod:
+	// 		mod name length
+	//		mod name
+	// 		major version
+	// 		minor version
+	// 		sub/patch version
+	// 		mod CRC
+	// startup mod settings - 00 for no mod settings
+
 	if (!globalData.connectionRequestIDGeneratedOnClient) return false
 	if (!globalData.connectionRequestIDGeneratedOnServer) return false
 
 	const ConnectionRequest = new Uint8Array([
-		0x44, 0x00, 0x00,
-		switchEndians(globalData.connectionRequestIDGeneratedOnClient), 
-		switchEndians(globalData.connectionRequestIDGeneratedOnServer), 
-		// other data later
+		0x24, 0x01, 0x00,
+		...numToU8(switchEndians(globalData.connectionRequestIDGeneratedOnClient)), 
+		...numToU8(switchEndians(globalData.connectionRequestIDGeneratedOnServer)),
+		...numToU8(switchEndians(globalData.instanceID)),
+		...numToU8(switchEndians(globalData.username.length)),
+		...stringToU8(globalData.username),
+		...numToU8(switchEndians(globalData.gamePassword.length)),
+		...stringToU8(globalData.gamePassword),
+		...numToU8(switchEndians(globalData.serverKey.length)),
+		...stringToU8(globalData.serverKey),
+		...numToU8(switchEndians(globalData.serverKeyTimestamp.length)),
+		...stringToU8(globalData.serverKeyTimestamp),
+		...numToU8(switchEndians(globalData.coreChecksum)),
+		...numToU8(switchEndians(globalData.prototypeListChecksum)),
+		...numToU8(switchEndians(globalData.mods.length)),
+		...globalData.mods.map(mod => {
+			const version = mod.version
+				.split(".")
+				.map(x=>parseInt(x))
+				.map(x=>switchEndians(x))
+				.map(x=>numToU8(x)).flat()
+			return [
+				...numToU8(switchEndians(mod.name.length)),
+				...stringToU8(mod.name),
+				...version.map(x=>switchEndians(x)).map(x=>numToU8(x)).flat(),
+				...numToU8(switchEndians(mod.crc))
+			]
+		}).flat(),
+		0x00 // some padding?
 	])
 
-	await listener.send(ConnectionRequest, {
-		hostname: REMOTE_ADDR,
-		port: REMOTE_PORT,
-		transport: "udp",
-	})
+	await listener.send(ConnectionRequest, REMOTE)
+}
+
+async function sendClientToServerHeartbeat(flags: number, extra?: number[]) {
+	const data = new Uint8Array([
+		0x06, // ClientToServerHeartbeat identification
+		flags,
+		...numToU8(globalData.sequenceNumberToSend++), // sequenceNumber
+		...numToU8(2**32 - 1), // nextToRecieveServerTickClosure
+		...extra || []
+	])
+	await listener.send(data, REMOTE)
 }
 
 await sendConnectionRequest()
 await sendConnectionRequestReplyConfirm()
+
+
+let hasNotReReplied = true
+for await (const [response] of listener) {
+	const type = response[0]
+	const typeString = type.toString(16)
+	if (typeString.slice(-1) === "7") {
+		// ServerToClientHeartbeat
+		if (response.includes(0x14) && hasNotReReplied) {
+			await sendConnectionRequestReplyConfirm()
+			hasNotReReplied = false
+		}
+	}
+	else if (typeString.slice(-1) === "5") {
+		// ConnectionAcceptOrDeny
+		if (!hasNotReReplied) {
+			// pass if it has not replied with a new connection request
+			let currentByteIndex = 0
+			currentByteIndex += 14 // go to game name length byte
+			const gameNameLengthByte = response[currentByteIndex]
+			currentByteIndex += gameNameLengthByte + 1 // go over game name length
+			const serverHashLength = response[currentByteIndex]
+			currentByteIndex += serverHashLength + 1 // skip over to description length
+			const descriptionLength = response[currentByteIndex]
+			currentByteIndex += descriptionLength + 2 // go to max updates byte
+			currentByteIndex += 4 + 8 + 1 // skip over game id, steam id & go to username length byte
+			currentByteIndex += response[currentByteIndex] + 2 // go to saving for byte
+
+			currentByteIndex += response[currentByteIndex] + 1 // go to peer info size
+			const peerInfoSize = response[currentByteIndex] // the amount of people connected?
+			currentByteIndex++ // move to first peer
+			for (let i = 0; i < peerInfoSize; i++) {
+				currentByteIndex++
+				const peerNameLength = response[currentByteIndex]
+				currentByteIndex += peerNameLength // skip to next peer
+			}
+			currentByteIndex += 6 // get to the sequence of what to start sending
+			// save the sequence number to start with into a variable
+			globalData.sequenceNumberToSend = switchEndians(u8ToNum(response.slice(currentByteIndex, currentByteIndex + 4)))
+
+			await sendClientToServerHeartbeat(0x10, [0x01, 0x03, 0x02])
+			// send a reply to the connection request
+			setInterval(() => sendClientToServerHeartbeat(0x00), 1/5)
+		}
+	}
+}
